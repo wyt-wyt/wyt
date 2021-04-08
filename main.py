d55e5e15eaf6b984c0498272f4c09569afcb3763
wyt <907355790@qq.com>
2021-04-08 23:00:59 +0800

    flask-script的作用

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask, redirect, url_for, request
+from werkzeug.routing import BaseConverter  # 导入转换器基类
+
+# 自定义正则转换器
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *args):
+        super(RegexConverter, self).__init__(url_map)
+        # 将接受的第1个参数当作匹配规则进行保存
+        self.regex = args[0]
+
 
 app = Flask(__name__)
+app.url_map.converters['re'] = RegexConverter
+
 
 @app.route('/success/<name>')
 def success(name):
@@ -14,6 +25,10 @@ def login():
     else:
         user = request.args.get('nm')
         return redirect(url_for('success', name = user))
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ study2.md
@@ -0,0 +1,10 @@
+flask-script的用法：
+
+~~~
+Flask-Script扩展提供向Flask插入外部脚本的功能。包括运行一个开发用的服务器，一个定制的 Python shell，设置数据库的脚本，cronjobs，以及其他的运行在web应用之外的命令行任务。
+
+Flask-Script和Flask本身的工作方式类似。只需要定义和添加能从命令行中被Manager实例调用的 命令即可。
+
+Flask-Script 的作用是可以通过命令行的方式来操作 Flask。例如通过命令跑一个开发版本的服务器、设置数据库、定时任务等。
+~~~
+
